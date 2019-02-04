7f7d5445da9a349c0c51833e642048bc21316511
Victor <victorlacorte@live.com>
2019-02-04 08:40:35 -0200

    Simple refactoring

--- tests/test_hours.py
+++ tests/test_hours.py
@@ -34,11 +34,11 @@ def test_toHoursMinutes(time, expected):
     assert toHoursMinutes(time) == expected
 
 @pytest.mark.parametrize('times, expected', [
-    (('3:07', '8:12', '-1:17', '15:03', '-0:27', '-16:00'), '8:38'),
+    (('3:07', '8:12', '-1:17', '15:03', '-0:27', '-16:00'), '8:38'), # jan
     (('3:07', '8:12', '14:44'), '26:03'),
     (('-1:17', '-0:27', '-16:00'), '-17:44'),
     (('26:03', '-17:44'), '8:19'),
-    (('8:38', '1:59'), '10:37'), # jan
+    (('8:38', '2:57', '-0:13'), '11:22'), # fev
 ])
 def test_myTimebank(times, expected):
     assert toHoursMinutes(timebank(times)) == expected
